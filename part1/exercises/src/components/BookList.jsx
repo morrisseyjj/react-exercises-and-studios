5ed7284cf625aca5c00ea36fadf07c7f9f3d3794
JJMoz <18526918+JJMoz@users.noreply.github.com>
2024-03-11 16:05:30 -0400

    ch. 29 exercise part 1

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Book List";
+   let book1 = "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1679496983i/62628727.jpg";
+   let book2 = "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1672574587i/60531406.jpg";
+   let book3 = "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1663935844i/61109069.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="Romantic Comedy by Curtis Sittenfield" />
+         <img src={book2} alt="Tress of the Emerald Sea by Brandon Sanderson" />
+         <img src={book3} alt="The London Seance Society by Sarah Penner" />
       </div>      
    );
 }
