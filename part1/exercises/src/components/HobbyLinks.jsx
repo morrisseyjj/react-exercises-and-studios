4d170c2640a4d94b16413309f845cb856080a2ac
JJMoz <18526918+JJMoz@users.noreply.github.com>
2024-03-11 20:29:10 -0400

    update

---
+++ part1/exercises/src/components/HobbyLinks.jsx
@@ -0,0 +1,16 @@
+export default function HobbyLinks() {
+    let hobbyLinks = ["https://www.hobbylobby.com/", "https://www.michaels.com/"];
+
+    return (
+        <ol>
+            <li><a href = {hobbyLinks[0]}> The Hobby Lobby Arts and Craft Store</a></li>
+            <li><a href = {hobbyLinks[1]}> Michaels Arts and Crafts</a></li>
+        </ol>
+    );
+}
+
+
+
+
+
+
