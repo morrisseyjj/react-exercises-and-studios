e50968bc613f012d1bda00a9a53d15fb58b9cdcb
JJMoz <18526918+JJMoz@users.noreply.github.com>
2024-03-21 16:18:25 -0400

    Exercises Ch. 31

--- part3/exercises/src/components/Introduction.jsx
+++ part3/exercises/src/components/Introduction.jsx
@@ -1,11 +1,11 @@
 export default function HobbyIntroduction () {
    return(
       <div>
-         <h2>3 Fun Facts About Needlepoint</h2>
+         <h2>3 Fun Facts About Reading</h2>
          <ol>
-            <li>Needlepoint uses a specialized canvas as it's base.</li>
-            <li>The most basic stitch is called a tent stitch.</li>
-            <li>Needlepoint uses a lot of different kinds of fibers.</li>
+            <li>Reading reduces stress by 68 percent.</li>
+            <li>Reading is the fastest wayto build vocabulary</li>
+            <li>Reading boosts mental stimulation and improves brain function</li>
          </ol>
       </div>
    );
